6a69c41e0a7ff55c3ca2bca05fca5292b6e80259
juhuntenburg <ju.huntenburg@gmail.com>
2015-08-26 19:18:49 +0200

    simplify fingerings

--- flute_piece_v1.py
+++ flute_piece_v1.py
@@ -43,10 +43,14 @@ bags['hold_breath'] = ['in middle', 'after in', 'after out']
 
 
 '''fingerings bag'''
-bags['fingering'] = ['trill keys 1', 'trill keys 2', '+thumb', '+thumb', '+thumb', 'trill keys 1+2',
-                     1, 2, 3, 4, 5, 6, 7, 8]
-# what is trill keys 1 or 2 referring to?
-# len should be 14
+bags['fingering'] = ['trill keys 1', 'trill keys 2', 'trill keys 1+2']
+# what are the numbers referring to?
+bags['fingering'] += 3*['+thumb'] #why 3*?
+bags['fingering'] += [x+1 for x in range(8)]
+
+print ""
+print "fingering"
+print bags['fingering']
 
 # additional list to implement the actual covering patterns
 # each list implements the 8 holes to cover, left to right
@@ -74,7 +78,6 @@ for i in range(len(covered)):
             sumlist=False
         print str(i+1)+choices[j]+": "+str(covered[i][choices[j]])+" "+str(lenlist)+" "+str(sumlist)
 
-# check length of each list and if they add up to index
 
 # IN / OUT
 
